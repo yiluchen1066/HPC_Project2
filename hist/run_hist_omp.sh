b1b956e10d9a21588bfbd65ea1edd79c994fa52b
Yilu Chen <chenyilu2016@outlook.com>
2021-03-21 17:44:41 +0100

    add run_hist_omp.sh

---
+++ hist/run_hist_omp.sh
@@ -0,0 +1,14 @@
+export OMP_NUM_THREADS=1
+./hist_omp
+
+export OMP_NUM_THREADS=2
+./hist_omp
+
+export OMP_NUM_THREADS=4
+./hist_omp
+
+export OMP_NUM_THREADS=8
+./hist_omp
+
+export OMP_NUM_THREADS=16
+./hist_omp
