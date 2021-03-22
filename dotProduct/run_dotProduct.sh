24773389d89e3670fed5c4bda965d2a5b17eb73e
Yilu Chen <chenyilu2016@outlook.com>
2021-03-22 16:20:10 +0100

    add run_dotProduct.sh

---
+++ dotProduct/run_dotProduct.sh
@@ -0,0 +1,17 @@
+export OMP_NUM_THREADS=1
+./dotProduct
+
+export OMP_NUM_THREADS=2
+./dotProduct
+
+export OMP_NUM_THREADS=4
+./dotProduct
+
+export OMP_NUM_THREADS=8
+./dotProduct
+
+export OMP_NUM_THREADS=16
+./dotProduct
+
+export OMP_NUM_THREADS=24
+./dotProduct
