7c191782772554c72905d2cb2d5b06186e034a0b
Yilu Chen <chenyilu2016@outlook.com>
2021-03-23 21:33:22 +0100

    add run_mandelomp.sh

---
+++ mandel_omp/run_mandelomp.sh
@@ -0,0 +1,14 @@
+export OMP_NUM_THREADS=1
+./mandel_omp
+
+export OMP_NUM_THREADS=2
+./mandel_omp
+
+export OMP_NUM_THREADS=4
+./mandel_omp
+
+export OMP_NUM_THREADS=8
+./mandel_omp
+
+export OMP_NUM_THREADS=16
+./mandel_omp
