a837051059c16cd51b841b1c77be74160ec1f7d7
Yilu Chen <chenyilu2016@outlook.com>
2021-03-24 14:34:17 +0100

    N for

--- dotProduct/dotProduct.cpp
+++ dotProduct/dotProduct.cpp
@@ -18,11 +18,12 @@
 // 13s ~800MB
 //#define N 100000000
 // 127s 16GB
-#define N 1000000000
+//#define N 1000000000
 #define EPSILON 0.1
 
-using namespace std;
 
+using namespace std;
+/*
 int main() {
   double time_serial, time_start = 0.0;
   double *a, *b;
@@ -118,4 +119,107 @@ int main() {
   delete[] b;
 
   return 0;
+}
+*/
+
+int main(){
+  for (int N = 100000; N < 1000000001; N = N*10)
+  {
+    double time_serial, time_start = 0.0;
+    double *a, *b;
+
+    // Allocate memory for the vectors as 1-D arrays
+    a = new double[N];
+    b = new double[N];
+
+    // Initialize the vectors with some values
+    for (int i = 0; i < N; i++) {
+      a[i] = i;
+      b[i] = i / 10.0;
+    }
+
+    long double alpha = 0;
+    // serial execution
+    // Note that we do extra iterations to reduce relative timing overhead
+    time_start = wall_time();
+    for (int iterations = 0; iterations < NUM_ITERATIONS; iterations++) {
+      alpha = 0.0;
+      for (int i = 0; i < N; i++) {
+        alpha += a[i] * b[i];
+      }
+    }
+    time_serial = wall_time() - time_start;
+    cout << "Serial execution time = " << time_serial << " sec" << endl;
+
+    long double alpha_parallel = 0;
+    double time_red = 0;
+    double time_critical = 0;
+
+    //   TODO: Write parallel version (2 ways!)
+    //   i.  Using reduction pragma
+    //   ii. Using  critical pragma
+
+    time_start = wall_time(); 
+    for (int iteration = 0; iteration < NUM_ITERATIONS; iteration++)
+    {
+      alpha_parallel = 0.0; 
+      #pragma omp parallel for schedule (static) shared (a, b) reduction (+: alpha_parallel)
+      for (int i = 0; i < N; i++)
+      {
+        alpha_parallel +=a[i]*b[i]; 
+      }
+      
+    }
+    time_red = wall_time() -time_start; 
+
+    if ((fabs(alpha_parallel - alpha) / fabs(alpha_parallel)) > EPSILON) {
+      cout << "parallel reduction: " << alpha_parallel << ", serial: " << alpha
+          << "\n";
+      cerr << "Alpha not yet implemented correctly!\n";
+      exit(1);
+    }
+
+    long double alpha_local;
+    time_start = wall_time(); 
+    #pragma omp parallel shared(alpha_parallel) private(alpha_local)
+    {
+      for (int iteration = 0; iteration < NUM_ITERATIONS; iteration++)
+      {
+
+        alpha_parallel = 0.0; 
+        alpha_local = 0.0; 
+        #pragma omp for schedule (static)
+        for (int i = 0; i < N; i++)
+        {
+          alpha_local += a[i]*b[i]; 
+        }
+        #pragma omp critical 
+        {
+          alpha_parallel += alpha_local; 
+        }
+      }
+
+    }
+    
+    time_critical=wall_time() - time_start; 
+    
+    if ((fabs(alpha_parallel - alpha) / fabs(alpha_parallel)) > EPSILON) {
+      cout << "parallel reduction: " << alpha_parallel << ", serial: " << alpha
+          << "\n";
+      cerr << "Alpha not yet implemented correctly!\n";
+      exit(1);
+    }
+    cout << "Parallel dot product = " << alpha_parallel
+        << " time using reduction method = " << time_red
+        << " sec, time using critical method " << time_critical << " sec"
+        << endl;
+
+    // De-allocate memory
+    delete[] a;
+    delete[] b;
+
+    return 0;
+
+  }
+  
 }
